f1f1ad13f6859c9a6ed4b3dc7b60109c0d6e7d3c
batmantis <batmantisex@gmail.com>
2016-06-16 13:21:59 -0400

    tinder cards sort of working

--- tff/www/js/controllers.js
+++ tff/www/js/controllers.js
@@ -1,22 +1,33 @@
 angular.module('app.controllers', [])
-  
-.controller('fightTabDefaultPageCtrl', function($scope) {
 
+.controller('fightTabDefaultPageCtrl', function($scope) {
+  $scope.cards = [
+    {image: 'img/TvTxmF70SounKc1nTzZG_17.jpg'},
+    {image: 'https://randomuser.me/api/portraits/men/23.jpg' }
+  ];
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.cardSwiped = function(index) {
+    var newCard = // new card data
+    $scope.cards.push(newCard);
+  };
 })
-   
+
 .controller('chatTabDefaultPageCtrl', function($scope) {
 
 })
-   
+
 .controller('settingsTabDefaultPageCtrl', function($scope) {
 
 })
-      
+
 .controller('loginCtrl', function($scope) {
 
 })
-   
+
 .controller('matchesTabDefaultPageCtrl', function($scope) {
 
 })
- 

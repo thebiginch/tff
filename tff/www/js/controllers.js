87d5f3ddc05dc62b113c998604bc3cb1516a13e3
batmantis <batmantisex@gmail.com>
2016-06-16 10:00:07 -0400

    tried pulling in the ionic creator again

---
+++ tff/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('fightTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('chatTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('settingsTabDefaultPageCtrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('matchesTabDefaultPageCtrl', function($scope) {
+
+})
+ 
---
+++ tff/www/js/routes.js
@@ -0,0 +1,65 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('tabsController.fightTabDefaultPage', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/fightTabDefaultPage.html',
+        controller: 'fightTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  .state('chatTabDefaultPage', {
+    url: '/page3',
+    templateUrl: 'templates/chatTabDefaultPage.html',
+    controller: 'chatTabDefaultPageCtrl'
+  })
+
+  .state('tabsController.settingsTabDefaultPage', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/settingsTabDefaultPage.html',
+        controller: 'settingsTabDefaultPageCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/page5',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('tabsController.matchesTabDefaultPage', {
+    url: '/page6',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/matchesTabDefaultPage.html',
+        controller: 'matchesTabDefaultPageCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/page1/page2')
+
+  
+
+});

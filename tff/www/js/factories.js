b82a3aea4451fd3d8ec0851e9f50f9ed5d84b2b7
batmantis <batmantisex@gmail.com>
2016-06-17 17:04:59 -0400

    match modal working

--- tff/www/js/factories.js
+++ tff/www/js/factories.js
@@ -1,15 +1,18 @@
-angular.module('app.factories', [])
+angular.module('app.factories', ['ionic'])
 
-.factory('fightFactory', function($http) {
+.factory('fightFactory', function($http, $ionicPopover) {
   return {
     getNewCards: function() {
       return $http.get('/api/users').then((users) => users.data)
     },
     cardSwipedRight: function(otherPerson) {
-      return $http.post('/api/matches',{userId: 60, personId: otherPerson.id, IR: true}).then((resp) => resp.data)
+      return $http.post('/api/matches',{userId: 1, personId: 3, IR: true})
+      .then((resp) => resp.data)
+      // .then((data) => )
     },
     cardSwipedLeft: function(otherPerson) {
       return $http.post('/api/matches',{userId: 50, personId: otherPerson.id, IR: false}).then((resp) => resp.data)
-    }
+    },
+
   }
 })

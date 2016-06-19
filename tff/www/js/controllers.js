6220cb6837830e78056f39fed15a879fd29965bb
batmantis <you@example.com>
2016-06-18 23:22:29 -0400

    really bad chat implementation on front end

--- tff/www/js/controllers.js
+++ tff/www/js/controllers.js
@@ -92,8 +92,52 @@ angular.module('app.controllers', ['app.factories', 'ionic', 'fsaPreBuilt', 'app
     });
 })
 
-.controller('chatTabDefaultPageCtrl', function($scope) {
+.controller('chatTabDefaultPageCtrl', function($scope, $timeout, $ionicScrollDelegate) {
 
+  $scope.hideTime = true;
+
+  var alternate,
+    isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
+
+  $scope.sendMessage = function() {
+    alternate = !alternate;
+
+    var d = new Date();
+    d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
+
+    $scope.messages.push({
+      userId: alternate ? '12345' : '54321',
+      text: $scope.data.message,
+      time: d
+    });
+
+    delete $scope.data.message;
+    $ionicScrollDelegate.scrollBottom(true);
+
+  };
+
+
+  $scope.inputUp = function() {
+    if (isIOS) $scope.data.keyboardHeight = 216;
+    $timeout(function() {
+      $ionicScrollDelegate.scrollBottom(true);
+    }, 300);
+
+  };
+
+  $scope.inputDown = function() {
+    if (isIOS) $scope.data.keyboardHeight = 0;
+    $ionicScrollDelegate.resize();
+  };
+
+  $scope.closeKeyboard = function() {
+    // cordova.plugins.Keyboard.close();
+  };
+
+
+  $scope.data = {};
+  $scope.myId = '12345';
+  $scope.messages = [];
 
 })
 

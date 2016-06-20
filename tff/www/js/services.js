bf0df1e2f4167e5e977f93f982c6525458458550
batmantis <batmantisex@gmail.com>
2016-06-19 21:56:11 -0400

    think this working somewhat

--- tff/www/js/services.js
+++ tff/www/js/services.js
@@ -1,7 +1,8 @@
 angular.module('app.services', [])
 
 .constant('config',{
-  apiUrl: 'http://192.168.1.35:1337'
+  // apiUrl: 'http://192.168.1.35:1337'
+  apiUrl: ''
 })
 
 .service('BlankService', [function(){
